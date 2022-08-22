369fd0e401160002e85c159fec2c23b911e47460
SeongbinPark <binni0515@gmail.com>
2022-08-22 16:13:34 +0900

    3-4. 응답 HTTP Status Code 제어(ServletUriComponentsBuilder, 헤더의 Location으로 URI반환)

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -2,8 +2,11 @@ package com.example.restfulwebservice.user;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,15 +24,26 @@ public class UserController {
     }
 
     //사용자 개별 조회
-    @GetMapping("/user/{id}")//이때 String인 id 가 @pathVariable의 int인 id로 자동변환
+    @GetMapping("/users/{id}")//이때 String인 id 가 @pathVariable의 int인 id로 자동변환
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
     }
 
     //사용자 추가
     @PostMapping("/users")
-    public User saveUser(@RequestBody User user) {//form형태가 아닌 JSON, XML같은 Object형태의 데이터를 받기 위해선 @RequestBody
-        return service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {//form형태가 아닌 JSON, XML같은 Object형태의 데이터를 받기 위해선 @RequestBody
+        User savedUser = service.save(user);
+
+        //응답코드를 제어 ->SUCB 사용. -> 서버에서 반환하려는 데이터를 ResponseEntity에 담아 전달
+        //SUCB를 통해서 '사용자추가'라는 요청된 작업을 완료 후 어떤 URI로 추가된 리소스를 확인가능한지 반환
+        //(상세정보보기가 가능한 URI 반환)
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")//이 {id}에 방금 만든 유저의 Id값.
+                .buildAndExpand(savedUser.getId())
+                .toUri();//결과를 Uri 형태로 변환 (반환타입 : URI)
+
+        return ResponseEntity.created(location).build();
+        //응답의 Location 헤더에 URI 반환.
     }
 
 }
