c8a6dca70a448f17405e456a172a62d796aa92a1
SeongbinPark <binni0515@gmail.com>
2022-08-22 21:57:16 +0900

    4-2. 다국어 처리를 위한 Internationalization 구현 방법(Accept-Language 헤더에 맞게 언어구현)

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,10 +1,17 @@
 package com.example.restfulwebservice.helloworld;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //    @RequestMapping(method = RequestMethod.GET, path = "/hello-word")
     @GetMapping("/hello-world")
@@ -23,4 +30,10 @@ public class HelloWorldController {
         //String.format(표현할 문자열, 들어갈 데이터);
     }
 
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        //기본은 한국어(@SpringBootApplication 참고)
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
 }
