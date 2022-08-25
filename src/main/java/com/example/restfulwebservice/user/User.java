b50ce3bd4bad828694b5c9967beff7a75660c6cd
SeongbinPark <binni0515@gmail.com>
2022-08-25 15:01:40 +0900

    6-4. JPA초기설정, H2연결, SQL직접 작성

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -4,10 +4,11 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -15,9 +16,13 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 //@JsonIgnoreProperties({"password", "ssn"})
 //@JsonFilter("UserInfo")//필터이름.(컨트롤러, 서비스클래스에서 사용가능)
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "이름은 2글자 이상 입력해주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT into user values(1, sysdate(), 'User1', 'testpw', '301010-1111111');
+INSERT into user values(1, sysdate(), 'User1', 'testpw', '301010-1111111');
+INSERT into user values(1, sysdate(), 'User1', 'testpw', '301010-1111111');
+INSERT into user values(1, sysdate(), 'User1', 'testpw', '301010-1111111');
+-- 쿼리를 직접 작성하여 넣을 수도 있다.
